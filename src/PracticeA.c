21a990b4c48b47e7daf913d066a5b5a85a46f7a7
yude <i@yude.jp>
2022-03-18 12:47:54 +0900

    Add PracticeA.c

---
+++ src/PracticeA.c
@@ -0,0 +1,13 @@
+// AtCoder Beginners Selection: PracticeA - Welcome to AtCoder
+// https://atcoder.jp/contests/abs/tasks/practice_1
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+    char s[334];
+    scanf("%d", &a);
+    scanf("%d %d", &b, &c);
+    scanf("%s", s);
+    
+    printf("%d %s\n", a + b + c, s);
+}
