31eb8a301c3908a6008790a5db7aaba6234e8eee
yude <i@yude.jp>
2022-03-18 13:04:39 +0900

    Add abc081_a.c

---
+++ src/abc081_a.c
@@ -0,0 +1,17 @@
+// ABC081A - Placing Marbles
+// https://atcoder.jp/contests/abs/tasks/abc081_a
+#include <stdio.h>
+
+int main() {
+    char s[3];
+    int count = 0;
+    scanf("%s", s);
+    
+    for (int i = 0; i < 3; i++) {
+        if (s[i] == '1') {
+            count++;
+        }
+    }
+    
+    printf("%d\n", count);
+}
